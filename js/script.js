08125cd338ce1f90f77191398e0cbf47ecac9319
Miftahul Ulum <134750804+miftahul05@users.noreply.github.com>
2024-09-05 13:36:58 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika hamburger di click
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+// klik di luar sidebar untuk menghilangkan humburger
+const hamburger = document.querySelector ('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+  if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+});
